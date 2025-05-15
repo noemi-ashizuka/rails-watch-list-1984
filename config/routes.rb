091d6fbfbe2f49aec58c2659ccd43b8017987074
noemi ashizuka <noemi.calanducci@gmail.com>
2025-05-15 18:27:43 +0900

    livecode + delete and partials

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+  resources :lists, only: [:index, :show, :create] do
+    resources :bookmarks, only: [:create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,26 @@
-require 'rest-client'
-require 'faker'
-
-puts 'Cleaning the db'
+require 'open-uri'
+puts "Cleaning the DB...."
 
 Bookmark.destroy_all
-List.destroy_all
 Movie.destroy_all
+List.destroy_all
 
-puts 'Cleaning done'
-
-puts 'Fetching genres'
-genres_url = "https://api.themoviedb.org/3/genre/movie/list?api_key=#{ENV['MOVIES_API']}&language=en-US"
-
-genres_doc = JSON.parse(RestClient.get(genres_url))
-
-genre_ids = genres_doc['genres'].map { |genre| genre['id'] }
-
-puts 'Genres created'
-
-puts 'Creating movies'
-
-
-movies = genre_ids.map do |genre_id|
-  url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['MOVIES_API']}&with_genres=#{genre_id}&sort_by=revenue.desc"
-
-  movies_doc = JSON.parse(RestClient.get(url))
-
-  movies_doc['results'].first(10)
-end
-
-movies.flatten.each do |movie_data|
-  movie = Movie.find_by(title: movie_data['title'])
-  unless movie
-    movie = Movie.create!(
-      title: movie_data['title'],
-      overview: movie_data['overview'],
-      poster_url: "https://image.tmdb.org/t/p/w500/#{movie_data['poster_path']}",
-      rating: movie_data['vote_average'].to_f.round(1)
+# the Le Wagon copy of the API
+puts "Creating movies.... \n"
+(1..5).to_a.each do |num|
+  url = "http://tmdb.lewagon.com/movie/top_rated?page=#{num}"
+  response = JSON.parse(URI.open(url).read)
+  
+  response['results'].each do |movie_hash|
+    puts "...creating the movie #{movie_hash['title']}..."
+    puts
+    # create an instance with the hash
+    Movie.create!(
+      poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+      rating: movie_hash['vote_average'],
+      title: movie_hash['title'],
+      overview: movie_hash['overview']
     )
   end
-  p "Creating #{movie.title}"
 end
-
-puts "There are #{Movie.count} movies in the database"
+puts "... created #{Movie.count} movies."
